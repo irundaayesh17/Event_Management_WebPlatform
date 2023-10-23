47f9a1b7b15ac6191150de195f24ce6eb64c2242
Ayesh Irunda <ayeshirunda1234@gmail.com>
2023-10-23 22:10:32 +0530

    5th commit

--- Home.js
+++ Home.js
@@ -1,10 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-    const logoutButton = document.getElementById("logout");
-
-    logoutButton.addEventListener("click", function () {
-        window.location.href = "Login.html"; // Redirect to the Login page
-    });
-});*/
 let submenu = document.getElementById("submenu")
         function toggleMenu(){
             submenu.classList.toggle("open-menu");
@@ -14,3 +7,31 @@ let submenu = document.getElementById("submenu")
             // Redirect to the Login.html page
             window.location.href = "Login.html";
         });
+// Get references to elements
+const createEventsLink = document.querySelector('.nav_link_events');
+const createEventsSubMenuLink = document.querySelector('.sub_menu_1 a');
+const eventContainer = document.querySelector('.event-container');
+const formCloseButton = document.querySelector('.form_close');
+const homeElement = document.querySelector('.home');
+
+// Function to open the event container
+function openEventContainer() {
+    eventContainer.style.display = 'block';
+    homeElement.classList.add('show');
+}
+
+// Function to close the event container
+function closeEventContainer() {
+    eventContainer.style.display = 'none';
+    homeElement.classList.remove('show');
+}
+
+// Event listener for clicking the "Create Events" link in the main menu
+createEventsLink.addEventListener('click', openEventContainer);
+
+// Event listener for clicking the "Create Events" link in the sub-menu
+createEventsSubMenuLink.addEventListener('click', openEventContainer);
+
+// Event listener for clicking the form close button
+formCloseButton.addEventListener('click', closeEventContainer);
+
